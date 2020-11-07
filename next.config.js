43d26650d99052217fb0c6bfb9eeecabf389f2de
Mauricio Yochizumi <mauricioyochizumi@MacBook-Pro-de-Mauricio-7.local>
2020-11-07 20:49:26 -0300

    feat(post): add md recosign && load postfile informations

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
-const withImages = require('next-images');
-module.exports = withImages({
-  esModule: true,
-});
+module.exports = {
+  target: 'serverless',
+  webpack: function (config) {
+    config.module.rules.push({
+      test: /\.md$/,
+      use: 'raw-loader',
+    })
+    return config
+  },
+}
+
+// const withImages = require('next-images');
+// module.exports = withImages({
+//   esModule: true,
+// })
