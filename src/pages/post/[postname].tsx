1d5a87696ed684d6c55b7ea44d03a057bc96b6b3
Mauricio Yochizumi <mauricioyochizumi@MacBook-Pro-de-Mauricio-7.local>
2020-11-07 20:50:50 -0300

    feat(post): add md recosign && load postfile informations

--- src/pages/post/[postname].tsx
+++ src/pages/post/[postname].tsx
@@ -1,5 +1,74 @@
-const post = () => {
-  return <h1>Post</h1>;
+import Link from 'next/link';
+import matter from 'gray-matter';
+import ReactMarkdown from 'react-markdown';
+import DefaultLayout from '../../shared/layout/DefaultLayout';
+import { NextPageContext } from 'next';
+
+interface IRequest {
+  frontmatter: {
+    title: string;
+    author: string;
+  };
+  markdownBody: string;
+}
+
+interface IBlogSlugContext {
+  keys: () => string[];
+}
+
+const post = ({ frontmatter, markdownBody }: IRequest) => {
+  if (!frontmatter.title) return <></>;
+
+  return (
+    <DefaultLayout pageTitle={frontmatter.title}>
+      <Link href="/">
+        <a> Back</a>
+      </Link>
+
+      <article>
+        <h1>{frontmatter.title}</h1>
+        <p>by {frontmatter.author}</p>
+
+        <div>
+          <ReactMarkdown source={markdownBody} />
+        </div>
+      </article>
+    </DefaultLayout>
+  );
 };
+
+export async function getStaticProps({ ...ctx }) {
+  const { postname } = ctx.params;
+
+  const content = await import(`../../posts/${postname}.md`);
+  const data = matter(content.default);
+
+  return {
+    props: {
+      frontmatter: data.data,
+      markdownBody: data.content
+    }
+  };
+}
+
+export async function getStaticPaths() {
+  const blogSlugs = (context: IBlogSlugContext) => {
+    const keys = context.keys();
+    const data = keys.map((key, index) => {
+      let slug = key.replace(/^.*[\\\/]/, '').slice(0, -3);
+
+      return slug;
+    });
+    return data;
+  };
+
+  const contexts = blogSlugs(require.context('../../posts', true, /\.md$/));
+  const paths = contexts.map(slug => `/post/${slug}`);
+
+  return {
+    paths,
+    fallback: false
+  };
+}
 
 export default post;
---
+++ src/posts/mypost.md
@@ -0,0 +1,10 @@
+---
+title: 'Title Here'
+author: 'Author here'
+---
+
+Some content here asdas dsad sad sads
+ad sadsa dasd
+asd sad asdsad sad
+asd asdsa dsad asd a
+asd asda sdsad asd sadsa das dsad as
