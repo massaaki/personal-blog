a81a57ea8d9e4493a82bc0abcbd9aff367f26bd3
Mauricio Yochizumi <mauricioyochizumi@MacBook-Pro-de-Mauricio-7.local>
2020-11-10 08:51:50 -0300

    feat(globalStyles): Added montserrat black font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -41,9 +41,9 @@ export default class MyDocument extends Document {
         <Head>
           <meta charSet="utf-8" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700;900&display=swap"
             rel="stylesheet"
-          ></link>
+          />
           {/* <link rel="icon" href="" /> */}
         </Head>
         <body>
