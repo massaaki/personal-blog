d2f504e98c63167ee7cebdc941ac8bc0fb893a33
Mauricio Yochizumi <mauricioyochizumi@MacBook-Pro-de-Mauricio-7.local>
2020-11-10 08:52:54 -0300

    feat(about): added HeaderSection

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,9 +1,17 @@
 import DefaultLayout from '../../shared/layout/DefaultLayout';
+import HeaderSection from 'shared/components/HeaderSection';
 
 const aboutPage = () => {
   return (
     <DefaultLayout pageTitle="About page">
-      <h1>About page</h1>
+      <div className="container">
+        <HeaderSection
+          topText="Hello, I'm"
+          mainText="Maurício Massaaki"
+          bottomText="Entrepeneur and passionate developer"
+        />
+        <h1>About page</h1>
+      </div>
     </DefaultLayout>
   );
 };
