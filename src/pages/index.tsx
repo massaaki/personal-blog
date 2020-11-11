456acb5b17b9a40e8d9ab29f48fba095c9c0eefd
Mauricio Yochizumi <mauricioyochizumi@MacBook-Pro-de-Mauricio-7.local>
2020-11-11 14:03:29 -0300

    refact: Removed console.logs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -37,13 +37,11 @@ export async function getStaticProps() {
   const posts = (context => {
     const keys: string[] = context.keys();
     const values = keys.map<{ default: string }>(context);
-    console.log('values..:', values);
 
     const data = keys.map((key, index) => {
       let slug = key.replace(/^.*[\\\/]/, '').slice(0, -3);
       const value = values[index];
 
-      console.log('type..:', typeof value.default);
       const document = matter(value.default);
       return {
         frontmatter: document.data,
