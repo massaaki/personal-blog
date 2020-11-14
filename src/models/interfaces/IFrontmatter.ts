32967c11ae7c60837340b57a2374391790b5eed5
Mauricio Yochizumi <mauricioyochizumi@MacBook-Pro-de-Mauricio-7.local>
2020-11-13 23:02:02 -0300

    feat(post): Added new parameters to post (minToRead, Level and Stacks)

--- src/models/interfaces/IFrontmatter.ts
+++ src/models/interfaces/IFrontmatter.ts
@@ -1,7 +1,17 @@
+import Level from '../enums/Level';
+import Stack from '../enums/Stack';
+
+interface Stacks {
+  name: Stack;
+}
 interface IFrontMatter {
   title: string;
   author: string;
+  publishDate?: Date;
+  minToRead?: number;
   thumbnail?: string;
+  level?: Level;
+  stacks?: Stacks[];
 }
 
 export default IFrontMatter;
