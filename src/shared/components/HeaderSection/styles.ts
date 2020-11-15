7ad62fd0c8a495def97fe07ec717a0e572e5355d
Mauricio Yochizumi <mauricioyochizumi@MacBook-Pro-de-Mauricio-7.local>
2020-11-15 14:28:39 -0300

    feat(headerSection): Add blockOfCode background

--- src/shared/components/HeaderSection/styles.ts
+++ src/shared/components/HeaderSection/styles.ts
@@ -5,6 +5,28 @@ export const Header = styled.section`
   height: 50vh;
   width: 100%;
   display: flex;
+
+  .codeBlock {
+    position: absolute;
+    display: flex;
+    align-items: center;
+    margin: auto;
+    right: 0px;
+    left: 60%;
+    top: 0;
+    bottom: 0;
+    > svg {
+      background: #f8f8f8;
+      padding: 15px 50px;
+      max-width: 460px;
+      width: 100%;
+      border-radius: 10px;
+      box-shadow: rgba(0, 0, 0, 0.2) 0px 5px 10px;
+    }
+    @media (max-width: 1000px) {
+      display: none;
+    }
+  }
 `;
 
 export const Content = styled.div`
